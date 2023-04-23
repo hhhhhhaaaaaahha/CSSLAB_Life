41dbc19fe1b230b62db3b9cd7f79ea394579642f
Ray <raytu.tpe@gmail.com>
2023-04-24 00:31:04 +0800

    connect user input with roulette option, but still got some drawing issue while options >= 7

--- roulette.py
+++ roulette.py
@@ -51,27 +51,17 @@ class Roulette(QWidget):
         # )
         # self.add_option_icon.setPixmap(QPixmap("./img/add_button.png").scaled(25, 25))
         # self.add_option_icon.move(500, 8)
-
-        self.opt = []
+        self.options: list[QLabel] = []
 
         self.text_field = QLineEdit(self)
         self.text_field.move(535, 10)
 
+        # Init submit button
         self.submit_button = QPushButton("新增", self)
         self.submit_button.clicked.connect(self.addOption)
         self.submit_button.move(670, 5)
         # icon = QIcon("img/add_button.png")
         # self.submit_button.setIcon(icon)
-
-    def addOption(self):
-        self.text_field.move(self.text_field.x(), self.text_field.y() + 30)
-        self.submit_button.move(self.submit_button.x(), self.submit_button.y() + 30)
-        self.opt.append(QLabel(f"{len(self.opt)+1}.    {self.text_field.text()}", self))
-        self.opt[-1].move(self.text_field.x(), self.text_field.y() - 30)
-        self.text_field.clear()
-        # self.temp = QLabel("QQQQQQQQQQQ", self)
-        # self.temp.move(self.text_field.x(), self.text_field.y() - 30)
-        self.opt[-1].show()
 
     def paintEvent(self, event):
         if self.mModified:
@@ -80,7 +70,7 @@ class Roulette(QWidget):
             self.drawRoulette(qp)
             self.mModified = False
 
-    def drawRoulette(self, qp):
+    def drawRoulette(self, qp: QPainter):
         pen = QPen(
             QColor(0, 0, 0),
             1,
@@ -96,32 +86,51 @@ class Roulette(QWidget):
             QColor(229, 57, 53),
             QColor(253, 216, 53),
             QColor(244, 81, 30),
+            QColor(253, 216, 53),
+            QColor(216, 27, 96),
+            QColor(255, 179, 0),
+            QColor(255, 252, 0),
         ]
+
         # Define option-related variables
-        option = ["I'm PASTA", "麵屋雞金", "自助餐"]
-        angles = np.arange(0, 360, 360 / (len(option) * 2), dtype=int)
-        points = self.calTextCoord(len(angles))
+        if len(self.options) < 2:
+            angles = [0]
+        else:
+            angles = np.arange(0, 360, 360 / (len(self.options) * 2), dtype=int)
+        if len(self.options) != 0:
+            points = self.calTextCoord(len(angles))
 
         # Draw the segments
-        for i in range(len(angles)):
-            qp.setBrush(QBrush(colors[i % len(option)], Qt.SolidPattern))
-            qp.drawPie(wheel_rect, angles[i] * 16 - self.angle, angles[1] * 16)
+        if len(self.options) < 2:
+            qp.save()
+            qp.setPen(Qt.NoPen)
+            qp.setBrush(QBrush(colors[0], Qt.SolidPattern))
+            qp.drawPie(wheel_rect, angles[0] * 16 - self.angle, 360 * 16)
+            qp.restore()
+        else:
+            for i in range(len(angles)):
+                qp.setBrush(QBrush(colors[i % len(self.options)], Qt.SolidPattern))
+                qp.drawPie(wheel_rect, angles[i] * 16 - self.angle, angles[1] * 16)
 
         # Draw text
-        for i in range(len(angles)):
-            qp.save()
-            qp.scale(1, 1)
-            qp.setFont(QFont("Academy Engraved LET", 25))
-            a = points[i, 2]
-            x, y = (self.text_radius * points[i, 0], self.text_radius * points[i, 1])
-            if points[i, 0] < 0:
-                a = a - np.pi
-            qp.drawText(
-                math.ceil(self.central_point.x() + x),
-                math.ceil(self.central_point.y() + y),
-                option[i % 3],
-            )
-            qp.restore()
+        if len(self.options) > 0:
+            for i in range(len(angles)):
+                qp.save()
+                qp.scale(1, 1)
+                qp.setFont(QFont("Academy Engraved LET", 25))
+                a = points[i, 2]
+                x, y = (
+                    self.text_radius * points[i, 0],
+                    self.text_radius * points[i, 1],
+                )
+                if points[i, 0] < 0:
+                    a = a - np.pi
+                qp.drawText(
+                    math.ceil(self.central_point.x() + x),
+                    math.ceil(self.central_point.y() + y),
+                    self.options[i % len(self.options)].text()[6:],
+                )
+                qp.restore()
 
         # Draw circles in the middle
         center_point = QPoint(200, 200)
@@ -133,6 +142,23 @@ class Roulette(QWidget):
         qp.drawEllipse(center_point, 35, 35)
         qp.setBrush(QBrush(QColor(117, 117, 117), Qt.SolidPattern))
         qp.drawEllipse(center_point, 25, 25)
+
+    def addOption(self):
+        # Move text field and submit button
+        self.text_field.move(self.text_field.x(), self.text_field.y() + 30)
+        self.submit_button.move(self.submit_button.x(), self.submit_button.y() + 30)
+
+        # Turn submitted text into label
+        self.options.append(
+            QLabel(f"{len(self.options)+1}.    {self.text_field.text()}", self)
+        )
+        self.options[-1].move(self.text_field.x(), self.text_field.y() - 30)
+        self.options[-1].show()
+        self.text_field.clear()
+
+        # Update window
+        self.mModified = True
+        self.update()
 
     def calTextCoord(self, N):
         arclen = ((2 * np.pi / N) * np.arange(0.5, N, 1)) + (

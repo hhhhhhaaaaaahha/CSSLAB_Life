65d655ba65d18a1a6bf953eeef0452f7d142ee06
Ray <raytu.tpe@gmail.com>
2023-04-28 21:14:01 +0800

    add .gitignore

---
+++ roulette_controller.py
@@ -0,0 +1,182 @@
+from PyQt5.QtCore import Qt, QPoint, QRectF
+from PyQt5.QtGui import QBrush, QColor, QFont, QPainter, QPen, QPolygon
+from PyQt5.QtWidgets import QLabel
+from roulette import Roulette
+
+import math
+import numpy as np
+import random
+
+from PyQt5.QtWidgets import QMainWindow
+from roulette_ui import RouletteUI
+
+
+class RouletteController(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.roulette = Roulette()
+        self.ui = RouletteUI()
+        self.ui.setupUI(self)
+        self.setInitUI()
+
+    def setInitUI(self):
+        self.ui.spin_button.clicked.connect(self.startTimer)
+        self.ui.submit_button.clicked.connect(self.addOption)
+        self.ui.timer.timeout.connect(self.onTimer)
+
+    def paintEvent(self, event):
+        if self.ui.mModified:
+            qp = QPainter()
+            qp.begin(self)
+            self.drawRoulette(qp)
+            self.ui.mModified = False
+
+    def drawRoulette(self, qp: QPainter):
+        pen = QPen(
+            QColor(0, 0, 0),
+            1,
+        )
+        qp.setPen(pen)
+
+        # Define the bounding rectangle for the wheel
+        wheel_rect = QRectF(80.0, 100.0, 380.0, 380.0)
+
+        # Define option-related variables
+        if self.roulette.getRestaurantCount() < 2:
+            angles = [0]
+        elif (
+            self.roulette.getRestaurantCount() < 7
+            and self.roulette.getRestaurantCount() >= 2
+        ):
+            angles = np.arange(
+                0, 360, 360 / (self.roulette.getRestaurantCount() * 2), dtype=int
+            )
+        else:
+            if 360 % self.roulette.getRestaurantCount() != 0:
+                ceiling = math.ceil(360 / self.roulette.getRestaurantCount())
+                angles = [
+                    i * ceiling for i in range(self.roulette.getRestaurantCount())
+                ]
+            else:
+                angles = np.arange(
+                    0, 360, 360 / self.roulette.getRestaurantCount(), dtype=int
+                )
+
+        if self.roulette.getRestaurantCount() != 0:
+            points = self.calTextCoord(len(angles))
+
+        # Draw the segments
+        if self.roulette.getRestaurantCount() < 2:
+            qp.save()
+            qp.setPen(Qt.NoPen)
+            qp.setBrush(QBrush(self.ui.colors[0], Qt.SolidPattern))
+            qp.drawPie(wheel_rect, angles[0] * 16 - self.ui.angle, 360 * 16)
+            qp.restore()
+        else:
+            for i in range(len(angles)):
+                qp.setBrush(
+                    QBrush(
+                        self.ui.colors[i % self.roulette.getRestaurantCount()],
+                        Qt.SolidPattern,
+                    )
+                )
+                qp.drawPie(wheel_rect, angles[i] * 16 - self.ui.angle, angles[1] * 16)
+
+        # Draw text
+        if self.roulette.getRestaurantCount() > 0:
+            for i in range(len(angles)):
+                qp.save()
+                qp.scale(1, 1)
+                qp.setFont(QFont("Academy Engraved LET", 25))
+                a = points[i, 2]
+                x, y = (
+                    self.ui.text_radius * points[i, 0],
+                    self.ui.text_radius * points[i, 1],
+                )
+                if points[i, 0] < 0:
+                    a = a - np.pi
+                qp.drawText(
+                    math.ceil(self.ui.central_point.x() + x),
+                    math.ceil(self.ui.central_point.y() + y),
+                    self.roulette.getRestaurant(i % self.roulette.getRestaurantCount()),
+                )
+                qp.restore()
+
+        # Draw circles in the middle and the triangular pointer
+        center_point = QPoint(270, 290)
+        qp.setPen(Qt.NoPen)
+        qp.setBrush(QBrush(QColor(117, 117, 117), Qt.SolidPattern))
+        qp.drawEllipse(center_point, 60, 60)
+
+        qp.drawPolygon(QPolygon([QPoint(210, 290), QPoint(330, 290), QPoint(270, 215)]))
+
+        qp.setBrush(QBrush(QColor(189, 189, 189), Qt.SolidPattern))
+        qp.drawEllipse(center_point, 35, 35)
+
+        qp.setBrush(QBrush(QColor(117, 117, 117), Qt.SolidPattern))
+        qp.drawEllipse(center_point, 25, 25)
+
+    def addOption(self):
+        # Add text in text field to restaurant_list
+        self.roulette.addRestaurant(self.ui.text_field.text())
+
+        # Move text field and submit button
+        self.ui.text_field.move(self.ui.text_field.x(), self.ui.text_field.y() + 30)
+        self.ui.submit_button.move(
+            self.ui.submit_button.x(), self.ui.submit_button.y() + 30
+        )
+
+        # Turn submitted text into label
+        self.ui.restaurant_labels.append(QLabel(f"{self.ui.text_field.text()}", self))
+        self.ui.restaurant_labels[-1].move(
+            self.ui.text_field.x(), self.ui.text_field.y() - 30
+        )
+        self.ui.restaurant_labels[-1].show()
+        self.ui.text_field.clear()
+
+        # Update window
+        self.ui.mModified = True
+        self.update()
+
+    def calTextCoord(self, n):
+        arclen = ((2 * np.pi / n) * np.arange(0.5, n, 1)) + (
+            (np.pi / 180) * (self.ui.angle / 16)
+        )
+        coordX = np.cos(arclen)
+        coordY = np.sin(arclen)
+        return np.c_[coordX, coordY, arclen]  # 每一個 row 代表一個點，其中三筆資料分別代表該點的 X 座標、Y 座標、
+
+    def startTimer(self):
+        self.ui.counter = 0
+        self.ui.spin_time = random.randint(2000, 2500)
+        self.ui.spin_angle = 60
+        self.ui.decrease_angle = self.ui.spin_angle / (self.ui.spin_time // 2)
+        self.ui.timer.start(1)
+
+    def onTimer(self):
+        if self.ui.counter < self.ui.spin_time:
+            if math.ceil(self.ui.spin_angle) == 0:
+                self.ui.spin_angle = 1
+                self.ui.decrease_angle = 0
+            if self.ui.counter < (self.ui.spin_time // 2):
+                self.ui.angle += self.ui.spin_angle
+            elif self.ui.counter >= (self.ui.spin_time // 2):
+                self.ui.spin_angle -= self.ui.decrease_angle
+                self.ui.angle += math.ceil(self.ui.spin_angle)
+            self.ui.counter += 1
+            self.ui.mModified = True
+            self.update()
+
+            # Check if chosen_option need to be update
+            pixel_color = self.grab().toImage().pixelColor(540, 400).getRgb()
+            if pixel_color[:3] != (0, 0, 0):
+                self.ui.chosen_option.setText(
+                    self.roulette.getRestaurantList()[::-1][
+                        self.ui.rgbs.index((pixel_color[:3]))
+                        - self.roulette.getRestaurantCount()
+                    ]
+                )
+            self.ui.mModified = True
+            self.update()
+        else:
+            self.ui.timer.stop()
--- roulette_ui.py
+++ roulette_ui.py
@@ -1,1 +1,95 @@
-from roulette import *
+from PyQt5.QtCore import Qt, QPoint, QTimer
+from PyQt5.QtGui import QColor, QFont
+
+from PyQt5.QtWidgets import (
+    QApplication,
+    QFrame,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QMainWindow,
+)
+
+import random
+
+
+class RouletteUI(object):
+    def setupUI(self, window: QMainWindow):
+        # Set window
+        self.mModified = True
+        window.setGeometry(100, 100, 800, 600)
+        window.setWindowTitle("Roulette")
+
+        # Define the colors for pie
+        self.rgbs = [
+            (195, 166, 211),
+            (215, 183, 232),
+            (237, 201, 255),
+            (246, 207, 243),
+            (254, 212, 231),
+            (248, 198, 195),
+            (242, 183, 159),
+            (236, 183, 132),
+            (229, 183, 105),
+            (223, 194, 79),
+            (220, 199, 66),
+            (216, 204, 52),
+            (220, 209, 70),
+        ]
+        random.shuffle(self.rgbs)
+        self.colors = [QColor(r, g, b) for r, g, b in self.rgbs]
+
+        # spin-related variables
+        self.angle = 0
+        self.decrease_angle = 0
+        self.counter = 1000000
+        self.spin_time = 100000
+        self.spin_angle = 30
+        self.timer = QTimer()
+
+        # text-related variables
+        self.text_radius = 150
+        self.central_point = QPoint(282, 298)
+
+        # Add spin button
+        self.spin_button = QPushButton("Spin", window)
+
+        ## Option-related
+        # Init option text field
+
+        # self.add_option_icon = QLabel(self)
+        # self.add_option_icon.setPixmap(
+        #     QPixmap("img/21a921f165298b0110b80821cc5bd761.svg").scaled(25, 25)
+        # )
+        # self.add_option_icon.setPixmap(QPixmap("./img/add_button.png").scaled(25, 25))
+        # self.add_option_icon.move(500, 8)
+        self.text_field = QLineEdit(window)
+        self.text_field.move(535, 10)
+        self.restaurant_labels: list[QLabel] = []
+
+        # Init chosen label
+        self.chosen_option = QLabel("???", window)
+        self.chosen_option.setFont(QFont("Academy Engraved LET", 30))
+        self.chosen_option.setAlignment(Qt.AlignCenter)
+        self.chosen_option.setFixedWidth(200)
+        self.chosen_option.setFrameShape(QFrame.Panel)
+        self.chosen_option.setFrameShadow(QFrame.Sunken)
+        self.chosen_option.setLineWidth(3)
+        self.chosen_option.move(189, 50)
+
+        # Init submit button
+        self.submit_button = QPushButton("新增", window)
+        self.submit_button.move(670, 5)
+        # icon = QIcon("img/add_button.png")
+        # self.submit_button.setIcon(icon)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    roulette_ui = RouletteUI()
+    roulette_ui.setupUI(window)
+    window.show()
+    sys.exit(app.exec_())

146e287ed2763dab17dad8a20da9fe2fd42c6155
Ray <raytu.tpe@gmail.com>
2023-04-29 03:29:36 +0800

    ignore __pycache__ in all folders

--- roulette/main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-from roulette_controller import RouletteController
-import sys
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    window = RouletteController()
-    window.show()
-    sys.exit(app.exec_())

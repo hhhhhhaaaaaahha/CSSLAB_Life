fcc97968c08da1f0318e666105a7a3bdb202c8b1
Ray <raytu.tpe@gmail.com>
2023-04-29 03:33:25 +0800

    create main.py as the starting point of 'CSSLAB Life' app

---
+++ main.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QApplication
+from controller.home_page_controller import HomePageController
+import sys
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = HomePageController()
+    window.show()
+    sys.exit(app.exec_())

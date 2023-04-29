2c144a237b7ebd9028604195e622fd2e006390fd
Ray <raytu.tpe@gmail.com>
2023-04-29 12:20:18 +0800

    connect all buttons to corresponding page

--- controller/home_page_controller.py
+++ controller/home_page_controller.py
@@ -1,6 +1,9 @@
 from PyQt5.QtWidgets import QMainWindow
 
 from controller.roulette_controller import RouletteController
+from controller.cleaning_time_table_controller import CleaningTimeTableController
+from controller.meeting_time_table_controller import MeetingTimeTableController
+from controller.school_time_table_controller import SchoolTimeTableController
 from ui.home_page_ui import HomePageUI
 
 
@@ -13,11 +16,27 @@ class HomePageController(QMainWindow):
 
     def setInitUI(self):
         self.roulette_window = RouletteController()
+        self.cleaning_time_table_window = CleaningTimeTableController()
+        self.meeting_time_table_window = MeetingTimeTableController()
+        self.school_time_table_window = SchoolTimeTableController()
+
         self.ui.pushButton.clicked.connect(self.changeToRoulette)
-        # self.ui.pushButton_2.clicked.connect(self.changeWindow)
-        # self.ui.pushButton_3.clicked.connect(self.changeWindow)
-        # self.ui.pushButton_4.clicked.connect(self.changeWindow)
+        self.ui.pushButton_2.clicked.connect(self.changeToCleaningTimeTable)
+        self.ui.pushButton_3.clicked.connect(self.changeToSchoolTimeTable)
+        self.ui.pushButton_4.clicked.connect(self.changeToMeetingTimeTable)
 
     def changeToRoulette(self):
         self.close()
         self.roulette_window.show()
+
+    def changeToCleaningTimeTable(self):
+        self.close()
+        self.cleaning_time_table_window.show()
+
+    def changeToMeetingTimeTable(self):
+        self.close()
+        self.meeting_time_table_window.show()
+
+    def changeToSchoolTimeTable(self):
+        self.close()
+        self.school_time_table_window.show()

9575a51afd867ced967c830e21de86c02ccaec88
Ray <raytu.tpe@gmail.com>
2023-05-02 15:58:05 +0800

    connect rtnbtn in cleaning_time_table_ui with event

--- controller/cleaning_time_table_controller.py
+++ controller/cleaning_time_table_controller.py
@@ -1,8 +1,13 @@
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QMainWindow
+
 from ui.cleaning_time_table_ui import CleaningTimeTableUI
 from src.cleaning_time_table import CleaningTimeTable
 
+
 class CleaningTimeTableController(QMainWindow):
+    backSignal = QtCore.pyqtSignal()
+
     def __init__(self):
         super().__init__()
 
@@ -10,4 +15,16 @@ class CleaningTimeTableController(QMainWindow):
         self.setWindowTitle("Cleaning Time Table")
 
         self.cleaningTimeTable = CleaningTimeTable()
-        self.ui = CleaningTimeTableUI(self, self.cleaningTimeTable.getMembers(), self.cleaningTimeTable.getSemesterStartD(), self.cleaningTimeTable.getSemesterEndD())
+        self.ui = CleaningTimeTableUI(
+            self,
+            self.cleaningTimeTable.getMembers(),
+            self.cleaningTimeTable.getSemesterStartD(),
+            self.cleaningTimeTable.getSemesterEndD(),
+        )
+
+        # Connect retbtn with return event
+        self.ui.retbtn.clicked.connect(self.changeToHomePage)
+
+    def changeToHomePage(self):
+        self.close()
+        self.backSignal.emit()
--- controller/home_page_controller.py
+++ controller/home_page_controller.py
@@ -16,12 +16,12 @@ class HomePageController(QMainWindow):
 
     def setInitUI(self):
         self.roulette_window = RouletteController()
-        # self.cleaning_time_table_window = CleaningTimeTableController()
+        self.cleaning_time_table_window = CleaningTimeTableController()
         self.meeting_time_table_window = MeetingTimeTableController()
         self.school_time_table_window = SchoolTimeTableController()
 
         self.ui.pushButton.clicked.connect(self.changeToRoulette)
-        # self.ui.pushButton_2.clicked.connect(self.changeToCleaningTimeTable)
+        self.ui.pushButton_2.clicked.connect(self.changeToCleaningTimeTable)
         self.ui.pushButton_3.clicked.connect(self.changeToSchoolTimeTable)
         self.ui.pushButton_4.clicked.connect(self.changeToMeetingTimeTable)
 
@@ -29,9 +29,10 @@ class HomePageController(QMainWindow):
         self.hide()
         self.roulette_window.show()
 
-    # def changeToCleaningTimeTable(self):
-    #     self.hide()
-    #     self.cleaning_time_table_window.show()
+    def changeToCleaningTimeTable(self):
+        self.cleaning_time_table_window.backSignal.connect(self.show)
+        self.hide()
+        self.cleaning_time_table_window.show()
 
     def changeToMeetingTimeTable(self):
         self.meeting_time_table_window.backSignal.connect(self.show)
